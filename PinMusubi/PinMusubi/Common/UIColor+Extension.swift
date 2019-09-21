4fc6a19e63ecc97aafe4864e1e6b175d97135b0b
naipaka <naipakapaka@gmail.com>
2019-09-21 16:55:33 +0900

    implement modal tableView

---
+++ PinMusubi/PinMusubi/Common/UIColor+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor+Extension.swift
+//  PinMusubi
+//
+//  Created by rMac on 2019/09/21.
+//  Copyright © 2019 naipaka. All rights reserved.
+//
+
+import Foundation
---
+++ PinMusubi/PinMusubi/View/SearchCriteriaCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchCriteriaCell.swift
+//  PinMusubi
+//
+//  Created by rMac on 2019/09/20.
+//  Copyright © 2019 naipaka. All rights reserved.
+//
+
+import UIKit
+
+class SearchCriteriaCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
