a9f575b23d27ce07956e4f409b988727d071aed6
naipaka <naipakapaka@gmail.com>
2019-10-03 21:34:38 +0900

    add image

--- PinMusubi/PinMusubi/Common/ColorDefinition.swift
+++ PinMusubi/PinMusubi/Common/ColorDefinition.swift
@@ -13,29 +13,29 @@ import UIKit
 public enum ColorDefinition {
     /// 設定地点の円の色
     public static let settingPointColors = [
-        UIColor(hex: "FA6400"), // オレンジ
+        UIColor(hex: "FA6400"), // 橙
         UIColor(hex: "26AA52"), // 緑
         UIColor(hex: "4284F4"), // 青
-        UIColor(hex: "FA6400"),
-        UIColor(hex: "26AA52"),
-        UIColor(hex: "4284F4"),
-        UIColor(hex: "FA6400"),
-        UIColor(hex: "26AA52"),
-        UIColor(hex: "4284F4"),
-        UIColor(hex: "FA6400")
+        UIColor(hex: "E74C3C"), // 赤
+        UIColor(hex: "F1C40F"), // 黄
+        UIColor(hex: "1ABC9C"), // 青緑
+        UIColor(hex: "9B59B6"), // 紫
+        UIColor(hex: "95A5A6"), // 銀
+        UIColor(hex: "34495E"), // 藍
+        UIColor(hex: "FF8080")  // 薄赤
     ]
 
     /// モーダル内のピンの背景色
     public static let underViewColorsOnModal = [
-        UIColor(hex: "FA6400", alpha: 0.16),    // オレンジ
+        UIColor(hex: "FA6400", alpha: 0.16),    // 橙
         UIColor(hex: "26AA52", alpha: 0.16),    // 緑
         UIColor(hex: "4284F4", alpha: 0.16),    // 青
-        UIColor(hex: "FA6400", alpha: 0.16),
-        UIColor(hex: "26AA52", alpha: 0.16),
-        UIColor(hex: "4284F4", alpha: 0.16),
-        UIColor(hex: "FA6400", alpha: 0.16),
-        UIColor(hex: "26AA52", alpha: 0.16),
-        UIColor(hex: "4284F4", alpha: 0.16),
-        UIColor(hex: "FA6400", alpha: 0.16)
+        UIColor(hex: "E74C3C", alpha: 0.16),    // 赤
+        UIColor(hex: "F1C40F", alpha: 0.16),    // 黄
+        UIColor(hex: "1ABC9C", alpha: 0.16),    // 青緑
+        UIColor(hex: "9B59B6", alpha: 0.16),    // 紫
+        UIColor(hex: "95A5A6", alpha: 0.16),    // 銀
+        UIColor(hex: "34495E", alpha: 0.16),    // 藍
+        UIColor(hex: "FF8080", alpha: 0.16)     // 薄赤
     ]
 }
