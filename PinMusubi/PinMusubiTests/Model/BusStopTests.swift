d44b85b919f3f690e906c76d0c3bc963fbb3b574
naipaka <naipakapaka@gmail.com>
2019-10-27 17:33:36 +0900

    implement test about bus stop model

--- PinMusubi/PinMusubiTests/Model/BusStopTests.swift
+++ PinMusubi/PinMusubiTests/Model/BusStopTests.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2019 naipaka. All rights reserved.
 //
 
-import Foundation
+import CoreLocation
+import XCTest
+@testable import PinMusubi
+
+class BusStopTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testFetchBusStopList_ok() {
+        let busStopModel = BusStopModel()
+        let fetchExpectation: XCTestExpectation? = self.expectation(description: "fetchBusStopList")
+        var examplePinPoint = CLLocationCoordinate2D()
+        examplePinPoint.latitude = 35.681236
+        examplePinPoint.longitude = 139.701636
+        busStopModel.fetchBusStopList(pinPoint: examplePinPoint, completion: { busStops, status in
+            XCTAssertEqual(status, ResponseStatus.success)
+            for busStop in busStops {
+                print("😆😆😆😆😆😆😆😆😆😆😆😆")
+                print(busStop)
+            }
+            fetchExpectation?.fulfill()
+        })
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+}
