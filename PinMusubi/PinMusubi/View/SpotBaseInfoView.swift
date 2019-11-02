8e581b38aa2b65151d3c982faa2cc295cb362904
naipaka <naipakapaka@gmail.com>
2019-11-02 23:16:57 +0900

    implement station view and busStation view

---
+++ PinMusubi/PinMusubi/View/SpotBaseInfoView.swift
@@ -0,0 +1,21 @@
+//
+//  SpotBaseInfoView.swift
+//  PinMusubi
+//
+//  Created by rMac on 2019/11/02.
+//  Copyright © 2019 naipaka. All rights reserved.
+//
+
+import UIKit
+
+class SpotBaseInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
